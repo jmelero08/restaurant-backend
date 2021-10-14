269177305b0f916f6439dd43e40393c69ac12dda
Jose Melero <jmelero08@gmail.com>
2021-10-13 18:17:13 -0600

    fixed some typos

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,19 +1,16 @@
 class Api::V1::RestaurantsController < ApplicationController
     before_action :set_restaurant, only: [:show, :update, :destroy]
 
-    # GET /restaurants
     def index
       @restaurants = Restaurant.all
   
       render json: @restaurants
     end
   
-    # GET /restaurants/1
     def show
       render json: @restaurant
     end
   
-    # POST /restaurants
     def create
       @restaurant = Restaurant.new(restaurant_params)
   
@@ -24,7 +21,6 @@ class Api::V1::RestaurantsController < ApplicationController
       end
     end
   
-    # PATCH/PUT /restaurants/1
     def update
       if @restaurant.update(restaurant_params)
         render json: @restaurant
@@ -33,18 +29,15 @@ class Api::V1::RestaurantsController < ApplicationController
       end
     end
   
-    # DELETE /restaurants/1
     def destroy
       @restaurant.destroy
     end
   
     private
-      # Use callbacks to share common setup or constraints between actions.
       def set_restaurant
         @restaurant = Restaurant.find(params[:id])
       end
   
-      # Only allow a trusted parameter "white list" through.
       def restaurant_params
         params.require(:restaurant).permit(:name, :description)
       end
